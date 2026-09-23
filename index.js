3c766b4753aa32a2eb7fae2e569e9c3e5fd4673a
LevarJo <ivanproh04@gmai.com>
2026-09-23 22:07:36 +0300

    добавил еще файлов

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const app = express();
+const PORT = 2000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// раздаем статические файлики
+app.use(express.static(path.join(__dirname, "public")));
+
+// перенаправление ссылок
+// app.get("/bot", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/html/bot/bot_main.html"));
+// });
+// app.get("/list", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/html/list.html"));
+// });
+// app.get("/waves", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/html/waves/waves_main.html"));
+// });
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Сайт запущен: http://localhost:${PORT}`);
+});
